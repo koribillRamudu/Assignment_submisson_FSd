68f8848ed595068e1bf4ce7fba24cb37154f84c0
koribillRamudu <sekharramudu9054@msitprogram.net>
2024-08-14 16:52:50 +0530

    Assignmetday3

---
+++ AssignmentDay3.js
@@ -0,0 +1,11 @@
+// history of java script
+// javascript was created in the 1995 by Brendan Eich while he was working at netscape communications corporation. 
+// it was initially designed to add interactivity to web pages in the netspcape navigator web browser.
+// the first name of javascript is mocha then there renamed as a livescript, later they changed to javascript become at that time java was become popularity.
+// java script has become important language in web development.there are lot frameworks and techologies such as : node.js,jscript.
+
+// Data Types in javascript
+// there are two types: primitive,object
+// in primitive there are different types: number,string,boolean,null,undefined,symbol
+// in object the data type which are not in primitive types then it is an object
+
